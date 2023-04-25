28ff113ca948f00dc0a2de341e31ac8410c10ba4
Johnny <johnnyomoseni100@gmail.com>
2023-04-25 02:31:13 +0100

    Minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,11 @@
 import Portfolio from "../../components/Portfolio";
 
-import { IconContext } from "react-icons/lib";
 import "/sass/main.scss";
 
 export default function Home() {
   return (
-    // <IconContext.Provider
-    //   value={{
-    //     className: "icon",
-    //     size: "30",
-    //     style: { stroke: "#ccc", verticalAlign: "middle" },
-    //   }}
-    // >
     <div className="wrapper">
       <Portfolio />
     </div>
-    // </IconContext.Provider>
   );
 }
